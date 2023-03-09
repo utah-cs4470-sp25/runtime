7c0ec109298576a949db77ab642eaebdfc83b3f2
Pavel Panchekha <me@pavpanchekha.com>
2023-03-09 09:50:35 -0700

    Print when jpl_main returns

--- runtime.c
+++ runtime.c
@@ -67,6 +67,9 @@ int main(int argc, char **argv) {
     printf("]})\n");
   }
   jpl_main(args);
+  if (getenv("JPLRTDEBUG")) {
+    printf("[debug] jpl_main returned cleanly\n");
+  }
   return 0;
 }
 
